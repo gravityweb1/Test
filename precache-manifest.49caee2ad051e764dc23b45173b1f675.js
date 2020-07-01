b59410a5dac24b1afad52d5872a35f6d75c2aab3
Tal P <tal@exploit.co.il>
2020-07-01 15:11:09 -0400

    Updates

---
+++ precache-manifest.49caee2ad051e764dc23b45173b1f675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3470b330b05fecbab00d14f7a11111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba6b35c4d0a62591213d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f83697a2bbc18ce17047",
+    "url": "/static/js/2.3dd3c6a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd3c6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6b35c4d0a62591213d",
+    "url": "/static/js/main.d2087fb6.chunk.js"
+  },
+  {
+    "revision": "7ac49d665222682dfee3",
+    "url": "/static/js/runtime-main.c5aae48b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
